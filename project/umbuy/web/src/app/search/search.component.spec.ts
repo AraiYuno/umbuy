70f624c8086bc70a57466e3a4963af5d83f0d6a3
YuanDAqua <umding8@myumanitoba.ca>
2018-02-11 15:25:13 -0600

    changed seach.unitTest file

--- project/umbuy/web/src/app/search/search.component.spec.ts
+++ project/umbuy/web/src/app/search/search.component.spec.ts
@@ -1,72 +1,47 @@
-/*import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {By} from '@angular/platform-browser';
 import { SearchComponent } from './search.component';
+import { HttpClient } from '@angular/common/http';
 import { AdvertisementService } from '../services/advertisement.service';
-import { ShareSearchResultService } from '../services/shareSearchResult.service'
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/observable/from';
-import 'rxjs/add/observable/empty';
-import 'rxjs/add/observable/throw';
-describe('SearchComponent', () => {
-    let component: SearchComponent; 
-    let adService: AdvertisementService;
-    let shareService: ShareSearchResultService;
-  
-    beforeEach(() => {
-      
-      adService= new AdvertisementService(null);
-      shareService = new ShareSearchResultService();
-      component= new SearchComponent(adService, shareService);
-  
-    });
-  
-    /*it('should call the server save the changed when the method is called ', () => {
-        //arrange
-   let spy=spyOn(adService, 'getSearchResultByTitle').and.callFake(t => {
-         return Observable.empty();
-     });
+import { FilterResultService } from '../services/filterResult.service';
 
-         component.getData();
-         
-          //assert
-         expect(spy).toHaveBeenCalled();
-  
-     });
+describe('SearchComponent', () => {
+  let component: SearchComponent;
+  let fixture: ComponentFixture<SearchComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ SearchComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SearchComponent);
+    component = fixture.componentInstance;
     
-    it('should get the result return from the server', () => {
-        let results={title: 'iphone',id:1};
-        //arrange
-      let spy=spyOn(service, 'getSearchResultByTitle').and.callFake(t => {
-         return Observable.from([results]);
-     });
-       //Act
-         component.getData();
-         console.log(component.result);
-    //assert
-        expect(component.result.indexOf(results)).toBeGreaterThan(-1);
-     });
+    const INI_Adevertisement = { 
+      id: 1,
+      userId: 1,
+      title: 'iphone',
+      description: 'A great iphone for a great price',
+      price: 200,
+      created_on: new Date('2018-02-02'),
+      last_updated: new Date('2018-02-02'),
+      deleted_on: null,
+      imageUrl: 'http',
+      category: 'education'
+    };
     
-  
-    it('should set message property if server return and error', () => {
-        let error='error';
-        //arrange
-      let spy=spyOn(adService, 'getSearchResultByTitle').and.returnValue(Observable.throw(error));
-       //Act
-        component.getData();
-        console.log(component.message);
-    //assert
-       expect(component.message).toBe(error);
-     });
-     
-     //test onKey() 
-    it('should return the title when onkey method is calling', () => {
-        let result=" ";
-        //Act
-        component.onKey(result);
-        
-   
-    //assert
-       expect(component.title).toBe("");
-     });
-});*/
-  
+  });
+
+  it('should able to put text when do keyup in the input field', () => {
+    let testing='iphone';
+    fixture.detectChanges();
+    let putInput = fixture.debugElement.query(By.css('.from-control'));
+    putInput.triggerEventHandler('keyUp', 'testing');
+    console.log(component.filteredAds);
+    expect(component.filteredAds).toContain('iphone');
+
+  });
+});
