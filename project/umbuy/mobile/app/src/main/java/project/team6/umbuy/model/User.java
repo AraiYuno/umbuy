fb0233f27e7e811bc6cd220fb3e0557706e4feff
AraiYuno <pyh2982@gmail.com>
2018-02-10 23:44:43 -0600

    nativeScript mobile setup

--- project/umbuy/mobile/app/src/main/java/project/team6/umbuy/model/User.java
+++
@@ -1,9 +0,0 @@
-package project.team6.umbuy.model;
-
-public class User {
-    private int id;
-    private String firstName;
-    private String lastName;
-    private String umEmail;
-    private String phoneNumber;
-}
---
+++ project/umbuy/mobile/app/view-ads/view-ads.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { Router } from '@angular/router';
+import * as ApplicationSettings from 'application-settings';
+
+@Component({
+    selector: "android-view-ads-component",
+    templateUrl: "./view-ads/view-ads.xml"
+})
+
+export class ViewAdsComponent{
+    private router: Router;
+    
+    // TODO: import Advertisement object from web app and share the services.
+} 
