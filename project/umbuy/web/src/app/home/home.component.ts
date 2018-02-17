c9f3819a4e3f3479d20dff84542048076f4d7cd2
Bhaulik <bhaulikpatel966@gmail.com>
2018-02-17 17:27:21 -0600

    redirect changes

--- project/umbuy/web/src/app/home/home.component.ts
+++ project/umbuy/web/src/app/home/home.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth/auth.service';
 
@@ -8,9 +9,19 @@ import { AuthService } from '../auth/auth.service';
 })
 export class HomeComponent implements OnInit {
   
-  constructor(public auth: AuthService) { }
+  constructor(public auth: AuthService, public router: Router) { }
 
   ngOnInit() {
     
+  }
+
+  login_check () {
+    if(this.auth.isAuthenticated()) {
+      return true;
+    }
+    else {
+      //this.router.navigate(['/']);
+      return false;
+    }
   }
 }
