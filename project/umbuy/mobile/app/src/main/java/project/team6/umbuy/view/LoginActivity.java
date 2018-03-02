963457b0765ceaf11b048d7670370448510ad1af
munsifb <kalmati14@gmail.com>
2018-03-02 14:37:50 -0600

    Added Mobile Signup with User Info Fields

--- project/umbuy/mobile/app/src/main/java/project/team6/umbuy/view/LoginActivity.java
+++ project/umbuy/mobile/app/src/main/java/project/team6/umbuy/view/LoginActivity.java
@@ -5,6 +5,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+import project.team6.umbuy.R;
+
 import com.auth0.android.Auth0;
 import com.auth0.android.authentication.AuthenticationAPIClient;
 import com.auth0.android.authentication.AuthenticationException;
@@ -15,7 +19,7 @@ import com.auth0.android.lock.LockCallback;
 import com.auth0.android.lock.utils.LockException;
 import com.auth0.android.result.Credentials;
 import com.auth0.android.result.UserProfile;
-
+import com.auth0.android.lock.utils.CustomField;
 import project.team6.umbuy.controller.CredentialsManager;
 
 
@@ -23,9 +27,25 @@ public class LoginActivity extends Activity {
 
     private Lock mLock;
 
+    ArrayList<CustomField> customFields = new ArrayList<>();
+
+    CustomField field1 = new CustomField(R.drawable.com_auth0_lock_ic_check_success, 0, "firstName", R.string.app_name);
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        ArrayList<CustomField> customFields = new ArrayList<>();
+
+        CustomField firstName = new CustomField(R.drawable.com_auth0_lock_ic_username, 0, "FirstName", R.string.FirstName);
+        CustomField lastName = new CustomField(R.drawable.com_auth0_lock_ic_username, 0, "LastName", R.string.LastName);
+        CustomField phone = new CustomField(R.drawable.com_auth0_lock_ic_mobile, 0, "phone", R.string.phone);
+
+        customFields.add(firstName);
+        customFields.add(lastName);
+        customFields.add(phone);
 
         Auth0 auth0 = new Auth0(this);
         auth0.setOIDCConformant(true);
@@ -34,12 +54,13 @@ public class LoginActivity extends Activity {
                 .withAudience(String.format("https://%s/userinfo", "team6.auth0.com"))
                 .withScope("openid offline_access")
                 .withScope("openid profile email")
+                .withSignUpFields(customFields)
                 //Add parameters to the builder
                 .build(this);
         String accessToken = CredentialsManager.getCredentials(this).getAccessToken();
         if (accessToken == null) {
             doLogin();
-            //return;
+            return;
         }
         else{
             AuthenticationAPIClient aClient = new AuthenticationAPIClient(auth0);
