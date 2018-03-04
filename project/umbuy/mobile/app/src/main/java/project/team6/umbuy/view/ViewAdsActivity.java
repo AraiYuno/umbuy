141007e7470d41cd1c792897c277c437fd0d32b7
Bhaulik <bhaulikpatel966@gmail.com>
2018-03-03 19:29:46 -0600

    major fix with the android xml to resolve authentication issue, navbar has home and logout

--- project/umbuy/mobile/app/src/main/java/project/team6/umbuy/view/ViewAdsActivity.java
+++ project/umbuy/mobile/app/src/main/java/project/team6/umbuy/view/ViewAdsActivity.java
@@ -143,7 +143,6 @@ public class ViewAdsActivity extends AppCompatActivity {
                                 return true;
                         }
 
-
                     }
                 });
 
