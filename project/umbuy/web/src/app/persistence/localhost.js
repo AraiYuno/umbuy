1f47a0359fded1e79cd3dbf3a035ee2c082c0e54
Stefan <stefc2013@gmail.com>
2018-03-06 14:53:51 -0600

    Changed callback back to localhost so Franklin can test locally

--- project/umbuy/web/src/app/persistence/localhost.js
+++ project/umbuy/web/src/app/persistence/localhost.js
@@ -12,8 +12,8 @@ app.use(bodyParser.urlencoded({extended: false}));
 /* This file is used for localhost testing */
 var connection = mysql.createConnection({
     host: '127.0.0.1',
-    user: 'coutures',
-    password: "cherry14",
+    user: 'yourusername',
+    password: "yourpassword",
     database: 'project4350',
     port: '3306'
 });
--- project/umbuy/web/src/app/shared/auth0-variables.ts
+++ project/umbuy/web/src/app/shared/auth0-variables.ts
@@ -7,6 +7,7 @@ interface AuthConfig {
   export const AUTH_CONFIG: AuthConfig = {
     clientID: 'FFM4DomrxSjVr4hWrP5JZly9j1jPE0d9',
     domain: 'team6.auth0.com',
-    callbackURL: 'http://ec2-18-217-86-148.us-east-2.compute.amazonaws.com:9000/callback'
+    callbackURL: 'http://localhost:4200/callback'
+    //callbackURL: 'http://ec2-18-217-86-148.us-east-2.compute.amazonaws.com:9000/callback'
   };
   
