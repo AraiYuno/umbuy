0eee6eaa72aac3365f95f7c742e6a486cfa7d123
Stefan <stefc2013@gmail.com>
2018-03-20 00:02:25 -0500

    updated acceptance tests for web to make the register tests more robust and more likely to pass

--- project/umbuy/web/e2e/pages/register.po.ts
+++ project/umbuy/web/e2e/pages/register.po.ts
@@ -1,6 +1,7 @@
-import { browser, by, element } from 'protractor';
+import { browser, by, element, protractor } from 'protractor';
 
 export class RegisterPage {
+  EC = protractor.ExpectedConditions;
   
   getHeader3Text() {
     return element(by.css('div div #signin h3')).getText();
@@ -15,43 +16,63 @@ export class RegisterPage {
   }
 
   clickSignUpButton(callback){
-    element(by.buttonText('SignUP')).click().then(function () {
+    var signUpBtn = element(by.buttonText('SignUP'));
+    browser.wait(this.EC.elementToBeClickable(signUpBtn), 9000);
+    signUpBtn.click().then(function () {
       callback();
     }); 
   }
 
   fillInSignUpForm(){
+    var emailElement = element(by.css('#email1'));
+    var passwordElement = element(by.css('#password1'));
+    var firstNameElement = element(by.css('#FirstName'));
+    var lastNameElement = element(by.css('#LastName'));
+    var phoneElement = element(by.css('#phone'));
+
+    browser.wait(this.EC.elementToBeClickable(emailElement), 9000);
+    browser.wait(this.EC.elementToBeClickable(passwordElement), 9000);
+    browser.wait(this.EC.elementToBeClickable(firstNameElement), 9000);
+    browser.wait(this.EC.elementToBeClickable(lastNameElement), 9000);
+    browser.wait(this.EC.elementToBeClickable(phoneElement), 9000);
+
     //generate a random email so there is no duplicates and the test will always pass.
     var email ="e2e" + this.getRandomNumber(0,600) + "test" + this.getRandomNumber(0,600) +
                   "fakeEmail" + this.getRandomNumber(0,600) + "softEng" +
                   this.getRandomNumber(0,600) + "@gmail.com";
     //fill email
-    element(by.css('#email1')).sendKeys(email);
+    emailElement.sendKeys(email);
     //fill password
-    element(by.css('#password1')).sendKeys("acceptancetest");
+    passwordElement.sendKeys("acceptancetest");
     //fill first name
-    element(by.css('#FirstName')).sendKeys("Team6");
+    firstNameElement.sendKeys("Team6");
     //fill last name
-    element(by.css('#LastName')).sendKeys("UMBUY");
+    lastNameElement.sendKeys("UMBUY");
     //fill phone number
-    element(by.css('#phone')).sendKeys("2049999999");
+    phoneElement.sendKeys("2049999999");
   }
 
   clickToSignUp(callback){
     //click sign up button
-    element(by.css('div div #signup form #btn-signup')).click().then(function () {
+    var signUpBtn = element(by.css('div div #signup form #btn-signup'));
+    browser.wait(this.EC.elementToBeClickable(signUpBtn), 9000);
+    signUpBtn.click().then(function () {
       browser.sleep(5000);
       callback();
     });
   }
 
   getAuthorizeModalText(){
-    return element(by.css('.first-line')).getText();
+    var firstLineElement = element(by.css('.first-line'));
+    browser.wait(this.EC.presenceOf(firstLineElement), 9000);
+    return firstLineElement.getText();
   }
 
   clickAuthorizeButton(){
+    var authorizeElement = element(by.css('#allow'));
+    browser.wait(this.EC.elementToBeClickable(authorizeElement), 9000);
     //click authorize button
-    element(by.css('#allow')).click().then(function () {
+    authorizeElement.click().then(function () {
       browser.sleep(5000);
     });
   }
