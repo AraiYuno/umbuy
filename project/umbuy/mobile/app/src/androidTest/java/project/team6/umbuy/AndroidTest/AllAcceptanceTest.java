4801f5354dca81fc8212f473b8d7355dcf998fd8
AraiYuno <pyh2982@gmail.com>
2018-03-22 19:35:58 -0500

    merged to devBranch

--- project/umbuy/mobile/app/src/androidTest/java/project/team6/umbuy/AndroidTest/AllAcceptanceTest.java
+++ project/umbuy/mobile/app/src/androidTest/java/project/team6/umbuy/AndroidTest/AllAcceptanceTest.java
@@ -3,7 +3,14 @@ package project.team6.umbuy.AndroidTest;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
-import project.team6.umbuy.AndroidTest.AcceptanceTest.AcceptanceTests;
+import project.team6.umbuy.AndroidTest.AcceptanceTest.CreateAdTest;
+import project.team6.umbuy.AndroidTest.AcceptanceTest.DelteAdsTest;
+import project.team6.umbuy.AndroidTest.AcceptanceTest.EditAdsTest;
+import project.team6.umbuy.AndroidTest.AcceptanceTest.LoginTest;
+import project.team6.umbuy.AndroidTest.AcceptanceTest.MyAdsTest;
+import project.team6.umbuy.AndroidTest.AcceptanceTest.ProfilePageTest;
+import project.team6.umbuy.AndroidTest.AcceptanceTest.ViewAdInfoTest;
+import project.team6.umbuy.AndroidTest.AcceptanceTest.ViewAdsTest;
 
 /**
  * Created by yuanding on 2018-03-17.
@@ -16,8 +23,16 @@ public class AllAcceptanceTest {
     public static Test suite()
     {
         suite = new TestSuite("Acceptance tests");
-        suite.addTest(AcceptanceTests.suite());
-        return suite;
+        suite.addTestSuite(LoginTest.class);
+        suite.addTestSuite(ViewAdsTest.class);
+        suite.addTestSuite(ViewAdInfoTest.class);
+        suite.addTestSuite(CreateAdTest.class);
+        suite.addTestSuite(ProfilePageTest.class);
+        suite.addTestSuite(MyAdsTest.class);
+        suite.addTestSuite(DelteAdsTest.class);
+        suite.addTestSuite(EditAdsTest.class);
 
+
+        return suite;
     }
 }
